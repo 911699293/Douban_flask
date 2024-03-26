5c50a40f8dbd05dc7099bf6ac5f625f8a9a31678
yuanxw <yxwgiser@163.com>
2024-03-26 22:19:58 +0800

    电影页面分页功能优化

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
-from flask import render_template
 import sqlite3
 
-# from sqlalchemy import create_engine, Metadata, Table, Column, String
-# from sqlalchemy.orm import sessionmaker
-# from flask_paginate import Paginate
+from flask import Flask, request
+from flask import render_template
+from flask_paginate import Pagination, get_page_parameter
+from sqlalchemy import create_engine, Table, Column, String, Integer, MetaData
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('mysql+pymysql://root:root123456@localhost:3306/douban250?charset=utf8mb4')
+DBSession = sessionmaker(bind=engine)
+metadata = MetaData()
+
+m_movies = Table(
+    'movie250', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('info_link', String()),
+    Column('pic_link', String()),
+    Column('cname', String()),
+    Column('ename', String()),
+    Column('score', String()),
+    Column('rated', String()),
+    Column('introduction', String()),
+    Column('info', String())
+)
 
 app = Flask(__name__)
 
@@ -16,17 +33,19 @@ def index():  # put application's code here
 
 
 @app.route('/movie')
-def movie():
-    datalist = []
-    con = sqlite3.connect('movie250.db')
-    cur = con.cursor()
-    sql = "SELECT * FROM movie250"
-    data = cur.execute(sql)
-    for row in data:
-        datalist.append(row)
-    cur.close()
-    con.close()
-    return render_template('movie.html', movies=datalist)
+def movie(page=None):
+    # 定义当前页码，从第一页开始
+    page = int(request.args.get('page', 1))
+    # 定义每一页的数量
+    per_page = 10
+    sess = DBSession()
+    movies = sess.query(m_movies).all()
+    pagination = Pagination(page=page, per_page=per_page, total=len(movies))
+    start = (page - 1) * per_page
+    end = start + per_page
+    movies = sess.query(m_movies).slice(start, end).all()
+    print(movies)
+    return render_template('movie.html', movies=movies, paginate=pagination)
 
 
 @app.route('/score')
